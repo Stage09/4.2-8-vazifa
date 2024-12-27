aa2b19bf6bc94cddd4e20aa94de23cb1a79f3757
Stage777999 <ikingib1979@gmail.com>
2024-12-27 16:05:17 +0500

    finished

---
+++ main.js
@@ -0,0 +1,27 @@
+// 1. https://the-winter.github.io/codingjs/exercise.html?name=missingChar&title=Warmup-1
+
+// function missingChar(str, n) {
+
+//     return str.slice(0, n) + str.slice(n + 1);
+//   }
+//   console.log(missingChar('kitten', 1)); 
+//   console.log(missingChar('kitten', 0)); 
+//   console.log(missingChar('kitten', 4));
+    
+// 2. https://the-winter.github.io/codingjs/exercise.html?name=notString&title=Warmup-1
+
+// function notString(str) {
+    
+//     if (str.startsWith("not")) {
+//       return str; 
+//     } else {
+//       return "not " + str; 
+//     }
+// }
+
+// console.log(notString('candy')); 
+// console.log(notString('x'));
+// console.log(notString('not bad'));
+
+
+3.
